c19e4ff0a88e10cf34d3f1a846cf3cc22cb22cc7
arno <ataljard01@gmail.com>
2017-06-24 15:49:50 +0200

    all tests pass

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,31 @@
-#write your code here
+def echo(greeting)
+  greeting
+end
+
+def shout(greeting)
+  greeting.upcase
+end
+
+def repeat(greeting, b=2)
+  ([greeting] * b).join(" ")
+end
+
+def start_of_word(s,n)
+  p s[0...n]
+end
+
+def first_word(s)
+  s.split(" ").first
+end
+
+def titleize(s)
+  words = s.split.map do |word|
+    if %w(the and over).include?(word)
+      word
+    else
+      word.capitalize
+    end
+  end
+  words.first.capitalize!
+  words.join(" ")
+end
